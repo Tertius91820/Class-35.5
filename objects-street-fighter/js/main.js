9a766778280aede24e1ef4c880bb17ee39ebc077
tertius Jubber <tjubber@gmail.com>
2023-06-24 13:37:37 +0200

    converted constructors to Classes

--- objects-street-fighter/js/main.js
+++ objects-street-fighter/js/main.js
@@ -1,18 +1,40 @@
-//Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
-function Streetfighter(name,age,specialMove,weapon){
-  this.name = name
-  this.age = age
-  this.specialMove = specialMove
-  this.weapon = weapon
+  //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+  function Streetfighter(name,age,specialMove,weapon){
+    this.name = name
+    this.age = age
+    this.specialMove = specialMove
+    this.weapon = weapon
+  
+    this.hiHit = function(){
+      console.log(`${this.name} throws a punch!`);
+    }
+    this.kick = function(){
+      console.log(`${this.name} kicks a low blow!`);
+    }
+    this.drawWeapon = function(){
+      console.log(`${this.name} draws his ${this.weapon} and does his ${this.specialMove}!!!`);
+    }
+  }
+    let goku = new Streetfighter('Goku',200,'fireball','sword')
 
-  this.hiHit = function(){
-    console.log(`${this.name} throws a punch!`);
+
+//Create a street fighter class that makes fighting game characters with 4 properties and 3 methods
+class Streetfighter1 {
+  constructor(name, age, specialMove, weapon) {
+    this.name = name
+    this.age = age
+    this.specialMove = specialMove
+    this.weapon = weapon;
   }
-  this.kick = function(){
-    console.log(`${this.name} kicks a low blow!`);
+  hiHit(){
+    console.log(`${this.name} throws a punch!`)
   }
-  this.drawWeapon = function(){
-    console.log(`${this.name} draws his ${this.weapon} and does his ${this.specialMove}!!!`);
+  kick(){
+    console.log(`${this.name} kicks a low blow!`)
   }
+  drawWeapon(){
+    console.log(`${this.name} draws his ${this.weapon} and does his ${this.specialMove}!!!`)
+  } 
 }
-  let goku = new Streetfighter('Goku',200,'fireball','sword')
+let goku1 = new Streetfighter1('Goku',200,'fireball','sword')
+
